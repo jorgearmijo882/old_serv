94d9781af30265fedfb4cbd0d6585ee3acdfba91
jorgeluis <jorgearmijo882@gmail.com>
2019-05-22 00:16:19 -0500

    Se incremento la salida del POST

--- src/main/java/com/devops/microservice/models/DevOps.java
+++ src/main/java/com/devops/microservice/models/DevOps.java
@@ -2,13 +2,24 @@ package com.devops.microservice.models;
 
 public class DevOps {
     private final String content;
+    private final String message = new String("This is a test");
+    private final String to = new String("Juan Perez");
+    private final String from = new String("Rita Asturia");
+    private final String timeToLifeSec = new String("45");
 
     public DevOps(String content) {
         this.content = content;
     }
-
-    public String getContent() {
-        return content;
+    public String getMessage() {
+        return message;
     }
-
+    public String getTo() {
+        return to;
+    }
+    public String getFrom() {
+        return from;
+    }
+    public String getTimeToLifeSec() {
+        return timeToLifeSec;
+    }
 }
